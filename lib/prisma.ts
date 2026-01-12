121e97710681098ff255cd8716730c569ef30c10
Altay Akyürek <altay123fbak@gmail.com>
2026-01-12 22:36:20 +0300

    Remove better-sqlite adapter from Prisma setup

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,15 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const adapter = new PrismaBetterSqlite3({
-    url: process.env.DATABASE_URL ?? 'file:./dev.db',
-});
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
 
 export const prisma =
-    globalForPrisma.prisma ||
+    globalForPrisma.prisma ??
     new PrismaClient({
-        adapter,
-        log: ['query'],
+        log: ['error'],
     });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
+}
